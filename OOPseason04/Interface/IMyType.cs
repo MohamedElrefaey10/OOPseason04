bc3d1b6040488a5507e10af79690c28f31ea7290
MoGa <MoGa@DESKTOP-HSMA4DI>
2025-01-30 21:58:29 +0200

    Intro to Interface With [Example 1]

---
+++ OOPseason04/Interface/IMyType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPseason04.Interface
+{
+    internal interface IMyType
+    {
+        //Default Access Modifier Inside Interface
+        // public
+        //what you can Write in Interface ?
+
+        //Signature for Property
+        int Salary { get; set; }
+
+
+        //Signature for Method
+        void myfun();
+
+
+        //Default Implemented Method => C# 8 .NETCore 3.1 [2019]
+
+        public void print()
+        {
+            Console.WriteLine("Hello From Default Implemented method");
+        }
+
+
+    }
+    class MyType : IMyType /*Implementation*/
+    {
+        //private int salary;
+        public int Salary { get; set;}//Automatic Prop
+
+        public void myfun()
+        {
+            Console.WriteLine("Hello Route");
+        }
+    }
+}
--- OOPseason04/Program.cs
+++ OOPseason04/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace OOPseason04
+﻿using System.Xml.Serialization;
+using OOPseason04.Interface;
+
+namespace OOPseason04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            #region Interface
+
+            //IMyType myType = new IMyType();
+
+            MyType myType = new MyType();
+
+            //myType.myfun();
+            //myType.Salary = 5000;
+
+            //myType.print();
             
+            IMyType myType1 = new MyType();
+
+            myType1.Salary = 6000;
+            myType1.myfun();
+            myType1.print();
+
+            #endregion
+
         }
     }
 }
